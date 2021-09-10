e7d9a676a03f17c2875354668812c2239dadab14
jelbaz-ledger <julien.el-baz@ledger.fr>
2021-09-10 14:42:04 +0200

    design pass

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,46 +1,34 @@
-import React, { Component } from 'react';
-import '../App.css';
+import React, { Component } from "react";
+import "../App.css";
 
 class header extends Component {
-
-    revokeAll = () => {
-        let buttons = document.getElementsByName('revoke');
-        for(let button of buttons) {
-            button.click();
-        }
+  revokeAll = () => {
+    let buttons = document.getElementsByName("revoke");
+    for (let button of buttons) {
+      button.click();
     }
+  };
 
-    render() {
-        return (
-            <div>
-                <div className="jumbotron">
-                    <div id="titles">
-                        <h1>CYAssets</h1>
-                        <h2>Find & revoke all the addresses that can spend your tokens</h2>
-                    </div>
-                </div>
-                <h3 id="loading" hidden>Loading, please wait...</h3>
-                <div id="results">
-                    <div className="grid-container">
-                        <div className="grid-item">Contract</div>
-                        <div className="grid-item">Approved Address</div>
-                        <div className="grid-item">Allowance</div>
-                        <div className="grid-item">Action</div>
-                    </div>
-                    <div id="revokeAll" hidden>
-                        <div className="grid-container">
-                            <div className="grid-items">ALL</div>
-                            <div className="grid-items">ALL</div>
-                            <div className="grid-items">ALL</div>
-                            <div className="grid-items">
-                                <button className="btn btn-danger" onClick={ this.revokeAll }> Revoke All Allowances</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <>
+        <div className="header" style={{ marginBottom: "2em" }}>
+          <p className="title">CYAssets</p>
+          <p className="subtitle">
+            Find and revoke all the addresses that can spend your tokens.
+          </p>
+        </div>
+        <div
+          className="subtitle"
+          id="loading"
+          hidden
+          style={{ margin: "2em 0" }}
+        >
+          Loading, please wait...
+        </div>
+      </>
+    );
+  }
 }
 
 export default header;
